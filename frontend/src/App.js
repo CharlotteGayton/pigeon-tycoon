db3a0f4cfb2cb3c5b7eec6e0b46d5b2fc032fd35
Charlotte Gayton <charlotte.gayton@gmail.com>
2024-08-15 12:49:52 +0100

    Add Chelsea Market font and PigeonList component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,202 +1,56 @@
-// import logo from './logo.svg';
-// import './App.css';
-
-// function App() {
-//   return (
-//     <div className="App">
-//       <header className="App-header">
-//         <img src={logo} className="App-logo" alt="logo" />
-//         <p>
-//           Edit <code>src/App.js</code> and save to reload.
-//         </p>
-//         <a
-//           className="App-link"
-//           href="https://reactjs.org"
-//           target="_blank"
-//           rel="noopener noreferrer"
-//         >
-//           Learn React
-//         </a>
-//       </header>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import './App.css';
+import PigeonList from './PigeonList';
+import useFetch from './UseFetch';
 
 function App() {
-    const [gameData, setGameData] = useState(null);
-    const [pigeons, setPigeons] = useState([]);
-    const [newPigeon, setNewPigeon] = useState({ name: '', speed: '', stamina: '' });
-    const [error, setError] = useState(null);
-    const [pigeonsForSale, setPigeonsForSale] = useState([]);
-    const [currentBalance, setCurrentBalance] = useState(0);
-
-    useEffect(() => {
-        axios.get('http://127.0.0.1:5000/api/game-data')
-            .then(response => {
-                setGameData(response.data);
-            })
-            .catch(error => {
-                console.error('There was an error fetching the game data!', error);
-            });
-    }, []);
-
-    useEffect(() => {
-      axios.get('http://127.0.0.1:5000/api/get-pigeons')
-          .then(response => {
-              setPigeons(response.data);
-              console.log(response.data);
-          })
-          .catch(error => {
-              console.error('There was an error fetching the game data!', error);
-          });
-  }, []);    
-
-    const saveGame = () => {
-        axios.post('http://127.0.0.1:5000/api/save-game', gameData)
-            .then(response => {
-                alert('Game saved!');
-            })
-            .catch(error => {
-                console.error('There was an error saving the game!', error);
-            });
-    };
-
-    const handleInputChange = (e) => {
-      const { name, value } = e.target;
-      setNewPigeon(prevState => ({
-          ...prevState,
-          [name]: value
-      }));
-  };
-
-    useEffect(() => {
-      axios.get('http://127.0.0.1:5000/api/get-pigeons-for-sale')
-          .then(response => {
-              setPigeonsForSale(response.data);
-          })
-          .catch(error => {
-              console.error('There was an error fetching the pigeons for sale!', error);
-          });
-        }, []); 
-
-    useEffect(() => {
-      axios.get('http://127.0.0.1:5000/api/get-balance')
-          .then(response => {
-              setCurrentBalance(response.data);
-          })
-          .catch(error => {
-              console.error('There was an error fetching the pigeons for sale!', error);
-          });
-        }, []); 
-
-    const addPigeon = () => {
-      axios.post('http://127.0.0.1:5000/api/add-pigeon', newPigeon)
-          .then(response => {
-              // Refresh pigeon list
-              axios.get('http://127.0.0.1:5000/api/get-pigeons')
-                  .then(response => {
-                      setPigeons(response.data);
-                      setNewPigeon({ name: '', speed: '', stamina: '' }); // Clear form
-                      setError(null); // Clear any previous errors
-                  });
-          })
-          .catch(error => {
-              setError(error.response?.data?.error || 'There was an error adding the pigeon');
-          });
-  };
+    const {data: pigeons, error:pigeonsError } = useFetch('http://127.0.0.1:5000/api/get-pigeons');
+    const {data: pigeonsForSale, error:pigeonsForSaleError } = useFetch('http://127.0.0.1:5000/api/get-pigeons-for-sale');
+    const {data: currentBalance, error:currentBalanceError } = useFetch('http://127.0.0.1:5000/api/get-balance');
 
     return (   
-
+        <div className="padding-container">
+            <div className="curved-box">
+                <div className="App">
+                    <header className="App-header">
+                        <h1>Pigeon Tycoon</h1>
+                    </header>
+                    <div class="page-break"></div>
+                    <div class="container">
+                        <div class="column">
+                            <div class="sub-column-container">
+                                <div class="column">
+                                    <div class="invisible-break"></div>
+                                    <div class="subtitle">Balance</div>
+                                </div>
+                                <div class="column">
+                                    <div class="invisible-break"></div>
+                                    <div class="subtitle">Income</div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="invisible-break"></div>
+                            <div class="subtitle">Enclosures</div>
+                            <div class="enclosure-box">
+                                <PigeonList pigeons={pigeons} />
+                            </div>
+                        </div>
+                        <div class="column">
+                            <div class="invisible-break"></div>
+                            <div class="subtitle">Stocks</div>
+                            <div class="invisible-break"></div>
+                            <div class="subtitle">Market</div>
+                            <div class="for-sale-box">
+                                <PigeonList pigeons={pigeonsForSale} />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
     );
-
-    // return (
-        // <div class="flex-container">
-        //     <div>1</div>
-        //     <div>2</div>
-        //     <div>3</div>
-        // </div>
-        // <div class="wrapper">
-        //     <article class="enclosure">
-        //         <p>
-        //             hooblydoobly
-        //         </p>
-        //     </article>
-        // </div>
-        // <ul class="navigation">
-        //     <li><a href="home">Home</a></li>
-        //     <li><a href="#">About</a></li>
-        //     <li><a href="#">Services</a></li>
-        //     <li><a href="#">Contact</a></li>
-        // </ul>
-    //   <div>
-    //       <h1>Pigeon List</h1>
-    //       {/* {pigeons.length === 0 ? (
-    //       <p>No pigeons available.</p> */}
-    //   {/* ) : ( */}
-    //       <ul>
-    //           {pigeons.map(pigeon => (
-    //               <li key={pigeon.id}>
-    //                   Name: {pigeon.name}, Speed: {pigeon.speed}, Stamina: {pigeon.stamina}
-    //               </li>
-    //           ))}
-    //       </ul>
-
-    //       <h2>Add a New Pigeon</h2>
-    //       <form onSubmit={(e) => { e.preventDefault(); addPigeon(); }}>
-    //           <div>
-    //               <label>Name:
-    //                   <input
-    //                       type="text"
-    //                       name="name"
-    //                       value={newPigeon.name}
-    //                       onChange={handleInputChange}
-    //                       required
-    //                   />
-    //               </label>
-    //           </div>
-    //           <div>
-    //               <label>Speed:
-    //                   <input
-    //                       type="number"
-    //                       name="speed"
-    //                       value={newPigeon.speed}
-    //                       onChange={handleInputChange}
-    //                       required
-    //                   />
-    //               </label>
-    //           </div>
-    //           <div>
-    //               <label>Stamina:
-    //                   <input
-    //                       type="number"
-    //                       name="stamina"
-    //                       value={newPigeon.stamina}
-    //                       onChange={handleInputChange}
-    //                       required
-    //                   />
-    //               </label>
-    //           </div>
-    //           <button type="submit">Add Pigeon</button>
-    //       </form>
-          
-    //       {error && <p style={{ color: 'red' }}>{error}</p>}
-    //       <ul>
-    //           {pigeonsForSale.map(pigeon => (
-    //               <li key={pigeon.id}>
-    //                   Name: {pigeon.name}, Speed: {pigeon.speed}, Stamina: {pigeon.stamina}
-    //               </li>
-    //           ))}
-    //       </ul>
-    //       <p>Current balance: {currentBalance.balance}</p>
-    //   </div>
-
-//   );
 }
 
 export default App;
